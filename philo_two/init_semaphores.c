95dbca4a8515a66fdf03a61fac845a3c0b84877c
Nath-G <nathalie.greselin@gmail.com>
2021-04-23 14:57:05 +0200

    update sem option

--- philo_two/init_semaphores.c
+++ philo_two/init_semaphores.c
@@ -6,7 +6,7 @@
 /*   By: nagresel <nagresel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/19 16:10:30 by nagresel          #+#    #+#             */
-/*   Updated: 2021/04/22 16:09:38 by nagresel         ###   ########.fr       */
+/*   Updated: 2021/04/23 14:45:52 by nagresel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	ft_init_sem_phi(char *sem_name, int phi, t_prog_dt *data)
 {
 	sem_unlink(ft_sem_name(sem_name, data->philo[phi].name));
 	data->philo[phi].meal_time = sem_open(ft_sem_name(sem_name,
-		data->philo[phi].name), O_CREAT | O_TRUNC | O_RDWR, S_IRWXU, 1);
+		data->philo[phi].name), O_CREAT | O_EXCL, 0777, 1);
 	if (data->philo[phi].meal_time == SEM_FAILED)
 		return (SEM_ERROR);
 	return (0);
@@ -27,16 +27,16 @@ int	init_data_sem(t_prog_dt *data)
 	int	i;
 
 	sem_unlink("/fork");
-	data->fork = sem_open("/fork", O_CREAT | O_EXCL, O_RDWR, data->n_philo);
+	data->fork = sem_open("/fork", O_CREAT | O_EXCL, 0777, data->n_philo);
 	sem_unlink("/end_eat");
-	data->finish_eaten = sem_open("/end_eat", O_CREAT | O_EXCL, O_RDWR,
+	data->finish_eaten = sem_open("/end_eat", O_CREAT | O_EXCL, 0777,
 		data->n_philo);
 	sem_unlink("/log_lck");
-	data->log_lock = sem_open("/log_lck", O_CREAT | O_EXCL, O_RDWR, 1);
+	data->log_lock = sem_open("/log_lck", O_CREAT | O_EXCL, 0777, 1);
 	if (data->log_lock == SEM_FAILED)
 		return (SEM_ERROR);
 	sem_unlink("/end_lck");
-	data->end_lock = sem_open("/end_lck", O_CREAT | O_EXCL, O_RDWR, 1);
+	data->end_lock = sem_open("/end_lck", O_CREAT | O_EXCL, 0777, 1);
 	if (data->end_lock == SEM_FAILED || data->log_lock == SEM_FAILED
 		|| data->fork == SEM_FAILED || data->finish_eaten == SEM_FAILED)
 	{
