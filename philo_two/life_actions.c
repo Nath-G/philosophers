f0859c74f9bde7a902dece628eed271fa8430d51
Nath-G <nathalie.greselin@gmail.com>
2021-04-23 12:34:04 +0200

    fix eat usleep bug

--- philo_two/life_actions.c
+++ philo_two/life_actions.c
@@ -6,7 +6,7 @@
 /*   By: nagresel <nagresel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/23 12:27:33 by nagresel          #+#    #+#             */
-/*   Updated: 2021/04/22 16:16:49 by nagresel         ###   ########.fr       */
+/*   Updated: 2021/04/23 12:22:03 by nagresel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,10 +44,7 @@ int			philo_eats(t_philo_dt *phi, t_prog_dt *dt)
 		ft_display_log((time_stamp / ONE_MLSEC), phi->name, " is eating\n",
 			dt);
 	sem_post(phi->meal_time);
-	ft_get_time(&cur_time);
-	if (!dt->is_finish)
-		usleep(dt->time_to_eat - ft_get_time_diff(&cur_time,
-			phi->time_last_meal));
+	usleep(dt->time_to_eat);
 	phi->meals_ate++;
 	if (phi->meals_ate == dt->n_meals)
 		sem_post(dt->finish_eaten);
@@ -64,11 +61,9 @@ int			philo_sleeps(t_philo_dt *phi, t_prog_dt *dt)
 	ft_get_time(&cur_time);
 	time_stamp = ft_get_time_diff(&cur_time, dt->time_start);
 	if (!dt->is_finish)
-	{
-		ft_display_log((time_stamp / ONE_MLSEC), phi->name, " is sleeping\n",
-			dt);
-		usleep(dt->time_to_sleep);
-	}
+		ft_display_log((time_stamp / ONE_MLSEC), phi->name,
+			" is sleeping\n", dt);
+	usleep(dt->time_to_sleep);
 	return (0);
 }
 
@@ -79,8 +74,8 @@ int			philo_thinks(t_philo_dt *phi, t_prog_dt *dt)
 
 	ft_get_time(&cur_time);
 	time_stamp = ft_get_time_diff(&cur_time, dt->time_start);
-	if (!dt->is_finish)
-		ft_display_log((time_stamp / ONE_MLSEC), phi->name, " is thinking\n",
-			dt);
+	if (!dt->is_finish && !dt->one_is_died)
+		ft_display_log((time_stamp / ONE_MLSEC), phi->name,
+			" is thinking\n", dt);
 	return (0);
 }
